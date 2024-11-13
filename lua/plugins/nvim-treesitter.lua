2ae4189ea5b8dbcaf7daf35a87c620004cf4e23a
Toon De Prins <toon.deprins@protonmail.com>
2024-11-13 19:24:23 +1100

    Added treesitter support

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,18 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+	-- need to use a config function here because it takes effect on .configs instead of the main plugin
+	config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "python", "fortran", "c", "cpp", "vim", "vimdoc", "html" },  -- must have installed parsers
+          sync_install = false,
+		  auto_install = true,  -- automatically install missing parser when entering buffer
+          highlight = { enable = true }, -- activate highlighting
+          indent = { enable = false },  -- activate indentation based on treesitter
+        })
+    end
+}
