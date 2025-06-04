9fc0fbcd0df1d167f17687e711fd14e2518db3ad
Toon De Prins <toon.deprins@protonmail.com>
2025-06-04 16:36:26 +1000

    Fixed duplicate LSP attachment in mason.lua

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -20,18 +20,20 @@ return {
             }
         })
 
-        -- set up interaction with lspconfig
-        mason_lspconfig.setup({
-            -- list of servers for mason to install
-            ensure_installed = {
-                "pyright", -- for Python programming
-                "ruff",
-                "fortls", -- for FORTRAN programming
-                "clangd", -- for C/C++ programming
-            },
-            -- auto-install configured servers (previously configured with lspconfig)
-            automatic_installation = true, -- not the same as ensure_installed
-        })
+        -- NOTE: this caused duplicate LSP attachments to buffers when upgrading to NEOVIM V11 -> I've commented
+        -- this out for now since it's not crucial.
+        ---- set up interaction with lspconfig
+        --mason_lspconfig.setup({
+        --    -- list of servers for mason to install
+        --    ensure_installed = {
+        --        "pyright", -- for Python programming
+        --        "ruff",
+        --        "fortls", -- for FORTRAN programming
+        --        "clangd", -- for C/C++ programming
+        --    },
+        --    -- auto-install configured servers (previously configured with lspconfig)
+        --    automatic_installation = true, -- not the same as ensure_installed
+        --})
 
         -- set up interaction with
     end,
